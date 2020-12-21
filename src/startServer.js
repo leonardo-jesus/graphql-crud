ce87c8b1e470861fb2c1ebe700cc0402d6c287f9
Leonardo Jesus <leonardojesus02@outlook.com>
2020-12-21 15:09:24 -0300

    feat: Add mongoose connection

--- src/startServer.js
+++ src/startServer.js
@@ -1,15 +1,12 @@
 import { ApolloServer } from 'apollo-server';
+import mongoose from 'mongoose';
 
 function startServer({ typeDefs, resolvers}) {
-  const MongoClient = require('mongodb').MongoClient;
-  const uri = "mongodb+srv://user:linkapi-graphql@graphql.4my99.mongodb.net/<dbname>?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
+  const connection = mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
   });
-
 
   const server = new ApolloServer({ typeDefs, resolvers });
   server.listen(2000).then(({ url }) => console.log(`🚀 Server started at ${url}`));
